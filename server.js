5bacd9c0f2d873077375e1c7a9d30bf5a670d002
estherkimyunjung <estherkimyunjung@gmail.com>
2021-04-07 03:17:37 -0500

    add pusher for backend

--- server.js
+++ server.js
@@ -2,10 +2,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
+import Pusher from "pusher";
 
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+  appId: "1184425",
+  key: "6d56966d7a92f137884a",
+  secret: "ace0d04c7869218760e1",
+  cluster: "us2",
+  useTLS: true,
+});
 
 // middleware
 app.use(express.json());
@@ -20,12 +29,46 @@ mongoose.connect(connection_url, {
   useUnifiedTopology: true,
 });
 
-// ???
+// pusher
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    // console.log("CHANGE", change);
+
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.user,
+        message: messageDetails.message,
+      });
+    } else {
+      console.log("Error triggring pusher");
+    }
+  });
+});
 
 // api routes 404-not found
-app.get("/", (req, res) => res.status(200).send("hello world"));
+app.get("/", (req, res) => res.status(200).send("hello world!!"));
 
-app.post("/messages/new", (req, res) => {
+app.get("/api/v1/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+      // 200 = success response ok
+    }
+  });
+});
+
+app.post("/api/v1/messages/new", (req, res) => {
   const dbMessage = req.body;
 
   Messages.create(dbMessage, (err, data) => {
@@ -39,6 +82,6 @@ app.post("/messages/new", (req, res) => {
   });
 });
 //listeners
-app.listen(port, () => console.log(`istening on location: ${port}`));
+app.listen(port, () => console.log(`istening on location: ${port}!!`));
 
 //admin YzVbfVDq0sBI4fBH - 75.53.226.63/32
